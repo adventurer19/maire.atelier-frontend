6b209c0c259ef8ea03bc89aa72562e35c9080e61
Nikolay Penchev <nikolay.penchev@ffwagency.com>
2025-10-15 16:13:11 +0300

    feat(frontend): bootstrap Next.js 15 + TS + Tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+       <div className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-600 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-xl">
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">
+          Tailwind работи! 🎉
+        </h1>
+        <p className="text-gray-600">Ако виждаш цветове и стилове, всичко е ОК!</p>
+      </div>
+    </div>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
